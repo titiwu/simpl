5fb89ff509a466a185d259ee117a2576efa9b5ef
Matthias Busl <matthias.busl@googlemail.com>
2018-12-28 23:34:48 +0100

    Cleanups after change to PyCharm

--- simpl/music_player.py
+++ simpl/music_player.py
@@ -11,11 +11,12 @@ import mpd
 import logging
 import time
 
+
 class MusicPlayer(object):
     _act_folder = ''
     _act_song_nr = 0
     _last_scan_time = 0
-    
+
     def __init__(self):
         self._logger = logging.getLogger(__name__)
         self._client = mpd.MPDClient()
@@ -26,17 +27,17 @@ class MusicPlayer(object):
         self._client.idletimeout = None
         self._client.connect("localhost", 6600)
         self._logger.debug("Connected to MPD " + self._client.mpd_version)
-    
+
     def get_status(self):
         """ Get the status update, but only when not done in the last 20 Seconds"""
-        if self._last_scan_time +20 < time.time():
+        if self._last_scan_time + 20 < time.time():
             mpd_status = self._client.currentsong()
             if 'pos' in mpd_status:
                 self._act_song_nr = int(mpd_status['pos'])
             else:
                 self._act_song_nr = 0
             if 'file' in mpd_status:
-                self._act_folder = mpd_status['file'].split('/', 1)[0] 
+                self._act_folder = mpd_status['file'].split('/', 1)[0]
             else:
                 self._act_folder = ''
 
@@ -47,7 +48,7 @@ class MusicPlayer(object):
     def get_act_folder(self):
         self.get_status()
         return self._act_folder
-        
+
     def play(self):
         """Play track"""
         self._client.play(0)
@@ -63,7 +64,7 @@ class MusicPlayer(object):
     def play_previous(self):
         """Play previous song"""
         self._client.previous()
-        
+
     def clear_playlist(self):
         self._client.clear()
 
@@ -79,7 +80,7 @@ class MusicPlayer(object):
         self._client.load(playlist)
         self.play()
 
-    def set_volume(self,  volume_prc):
+    def set_volume(self, volume_prc):
         """Set the playback volume (0-100)"""
         self._client.setvol(volume_prc)
 
@@ -90,18 +91,18 @@ class MusicPlayer(object):
         for dir in root_dirs:
             if 'directory' in dir:
                 self._client.lsinfo(dir['directory'])
-                file_list = self.get_files(dir['directory'],  'file')
-                playlist_list = self.get_files(dir['directory'],  'playlist')
-                dir_list.append({'directory': dir['directory'], 'files': file_list,  'playlists': playlist_list})
-        return(dir_list)
+                file_list = self.get_files(dir['directory'], 'file')
+                playlist_list = self.get_files(dir['directory'], 'playlist')
+                dir_list.append({'directory': dir['directory'], 'files': file_list, 'playlists': playlist_list})
+        return (dir_list)
 
-    def get_files(self,  directory,  type):
+    def get_files(self, directory, type):
         files = self._client.lsinfo(directory)
         file_list = list()
         for dir in files:
             if type in dir:
-                file_list.append(dir[type].replace(directory+'/',  ''))
-        return(file_list)
+                file_list.append(dir[type].replace(directory + '/', ''))
+        return (file_list)
 
     def __del__(self):
         self._client.close()
@@ -114,5 +115,5 @@ if __name__ == "__main__":
     print(music_player._client.status())
     print(music_player.get_act_song_nr())
     print(music_player.get_act_folder())
-    #print(music_player.switch_to_playlist('04-Radio/bayern_2.m3u'))
-    del(music_player)
+    # print(music_player.switch_to_playlist('04-Radio/bayern_2.m3u'))
+    del (music_player)
--- simpl/simpl_state.py
+++ simpl/simpl_state.py
@@ -7,9 +7,10 @@ Created on Thu Jan 19 21:42:12 2017
 """
 import logging
 
-import text_to_speech
-import music_player
-import music_directories
+from. import text_to_speech
+from. import music_player
+from. import music_directories
+
 
 class SimplState(object):
     """Base  State for simpl music player
@@ -24,24 +25,24 @@ class SimplState(object):
     _is_playing = False
     MAXIMUM_VOLUME = 100
     MINIMUM_VOLUME = 10
-    
+
     def __init__(self):
         self._logger = logging.getLogger(__name__)
-        self._speaker = text_to_speech.TextToSpeech() 
+        self._speaker = text_to_speech.TextToSpeech()
         self._music_player = music_player.MusicPlayer()
         self._directories = music_directories.SimplDirectories(self._music_player.get_directories())
-        #Init volume
+        # Init volume
         self._music_player.set_volume(self._act_vol)
         self.say('Simpl Mjusik Pläia ist bereit')
-   
-    def nextState(self,cls):
+
+    def nextState(self, cls):
         """Switching to next state"""
-        self._logger.debug('-> %s' % (cls.__name__,),)
+        self._logger.debug('-> %s' % (cls.__name__,), )
         self.__class__ = cls
 
     def showState(self):
         """Print actual state"""
-        self._logger.debug('%s' % (self.__class__.__name__),)
+        self._logger.debug('%s' % (self.__class__.__name__), )
 
     def cbButtonRiseVolume(self):
         """Rise playback volume"""
@@ -63,10 +64,10 @@ class SimplState(object):
 
     def cbButtonPlay(self):
         self._logger.debug('Play button pressed')
-  
+
     def cbButtonStop(self):
         self._logger.debug('Stop button pressed')
- 
+
     def cbButtonNext(self):
         self._logger.debug('Next button pressed')
 
@@ -74,13 +75,14 @@ class SimplState(object):
         self._logger.debug('Last button pressed')
 
     def cbButtonNumber(self, number):
-       self._logger.debug('Button playlist number '+str(number)+' pressed')
+        self._logger.debug('Button playlist number ' + str(number) + ' pressed')
 
-    def say(self,  full_text):
+    def say(self, full_text):
         """ Wrapper for Text to speech class."""
         self._music_player.pause()
         self._speaker.say(full_text)
         self._music_player.pause()
+
 
 class StatePlaying(SimplState):
     def cbButtonPlay(self):
@@ -100,16 +102,16 @@ class StatePlaying(SimplState):
         # Say next song nr
         max_nr = self._directories.get_nr_of_folder_entries(self._act_folder_nr)
         act_nr = self._music_player.get_act_song_nr()
-        next_song_nr = (act_nr+1)%(max_nr+1)
+        next_song_nr = (act_nr + 1) % (max_nr + 1)
         self.say(str(next_song_nr))
         self._music_player.play_next()
- 
+
     def cbButtonPrevious(self):
         super(StatePlaying, self).cbButtonPrevious()
         # Say previous song nr
         max_nr = self._directories.get_nr_of_folder_entries(self._act_folder_nr)
         act_nr = self._music_player.get_act_song_nr()
-        previous_song_nr = (act_nr-1)%(max_nr+1)
+        previous_song_nr = (act_nr - 1) % (max_nr + 1)
         self.say(str(previous_song_nr))
         self._music_player.play_previous()
 
@@ -118,31 +120,36 @@ class StatePlaying(SimplState):
         if not self._directories.folder_exists(number):
             self.say("Keine Musik hinterlegt")
         elif (number == self._act_folder_nr):
-            #self.say("Spielt schon")
+            # self.say("Spielt schon")
             pass
         else:
             self._act_folder_nr = number
             self._act_song_nr = 0
             self.say(self._directories.get_text_for_folder(self._act_folder_nr))
             if self._directories.is_radio_folder(self._act_folder_nr):
-                self.say(self._directories.get_radio_playlist_text(self._act_folder_nr,  self._act_song_nr))
-                self._music_player.switch_to_playlist(self._directories.get_radio_uri_for_mpd(self._act_folder_nr,  self._act_song_nr))
+                self.say(self._directories.get_radio_playlist_text(self._act_folder_nr, self._act_song_nr))
+                self._music_player.switch_to_playlist(
+                    self._directories.get_radio_uri_for_mpd(self._act_folder_nr, self._act_song_nr))
                 self.nextState(StatePlayingRadio)
             else:
                 self._music_player.switch_to_folder(self._directories.get_folder_uri_for_mpd(self._act_folder_nr))
                 self.nextState(StatePlaying)
             self._music_player.play()
 
+
 class StatePlayingRadio(StatePlaying):
     """A radio playlist is being played"""
+
     def cbButtonNext(self):
         super((self.__class__.__bases__)[0], self).cbButtonNext()
-        self._act_song_nr  = self._directories.get_next_radio_playlist_nr(self._act_folder_nr,  self._act_song_nr)
-        self.say(self._directories.get_radio_playlist_text(self._act_folder_nr,  self._act_song_nr))
-        self._music_player.switch_to_playlist(self._directories.get_radio_uri_for_mpd(self._act_folder_nr,  self._act_song_nr))
+        self._act_song_nr = self._directories.get_next_radio_playlist_nr(self._act_folder_nr, self._act_song_nr)
+        self.say(self._directories.get_radio_playlist_text(self._act_folder_nr, self._act_song_nr))
+        self._music_player.switch_to_playlist(
+            self._directories.get_radio_uri_for_mpd(self._act_folder_nr, self._act_song_nr))
 
     def cbButtonPrevious(self):
         super((self.__class__.__bases__)[0], self).cbButtonPrevious()
-        self._act_song_nr  = self._directories.get_previous_radio_playlist_nr(self._act_folder_nr,  self._act_song_nr)
-        self.say(self._directories.get_radio_playlist_text(self._act_folder_nr,  self._act_song_nr))
-        self._music_player.switch_to_playlist(self._directories.get_radio_uri_for_mpd(self._act_folder_nr,  self._act_song_nr))
+        self._act_song_nr = self._directories.get_previous_radio_playlist_nr(self._act_folder_nr, self._act_song_nr)
+        self.say(self._directories.get_radio_playlist_text(self._act_folder_nr, self._act_song_nr))
+        self._music_player.switch_to_playlist(
+            self._directories.get_radio_uri_for_mpd(self._act_folder_nr, self._act_song_nr))
